436e7a184234cf8bda2b6b6aee242260f06a38fb
Muhammad Bilal <imbilalen@gmail.com>
2026-06-17 19:52:27 +0500

    Update app.py

--- app.py
+++ app.py
@@ -205,6 +205,6 @@ def analyze():
         return jsonify({'error': f'Server error during prediction: {str(e)}'}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
-    #port = int(os.environ.get("PORT", 5000))
-    #app.run(host='0.0.0.0', port=port, debug=False)
+    #app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
